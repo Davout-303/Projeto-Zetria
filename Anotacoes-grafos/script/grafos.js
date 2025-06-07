be748281f1499b18e5219247d9c3159a8d4ef5c9
DidntThinkAName <davibrito494@gmail.com>
2025-06-07 11:16:11 -0300

    Teste no git

--- Anotacoes-grafos/script/grafos.js
+++ Anotacoes-grafos/script/grafos.js
@@ -5,16 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
         { id: 1, label: "Primeira guerra", shape: "hexagon", color: { background: "#FFE600", border: "#FFE600" } },
         { id: 2, label: "Segunda guerra", shape: "hexagon", color: { background: "#FFE600", border: "#FFE600" } },
         { id: 3, label: "Imperio Otomano", shape: "hexagon", color: { background: "#FFE600", border: "#FFE600" } },
-        { id: 4, label: "Tralalelo tralala", shape: "hexagon", color: { background: "#FFE600", border: "#FFE600" } },
-        { id: 5, label: "Bombardiro crocodilo", shape: "hexagon", color: { background: "#FFE600", border: "#FFE600" } },
-        { id: 6, label: "Jacaré vá pra casa", shape: "hexagon", color: { background: "#FFE600", border: "#FFE600" } },
+        { id: 4, label: "O cerco de Viena", shape: "hexagon", color: { background: "#FFE600", border: "#FFE600" } },
+        { id: 5, label: "Platão", shape: "hexagon", color: { background: "#FFE600", border: "#FFE600" } },
+        { id: 6, label: "Aristóteles", shape: "hexagon", color: { background: "#FFE600", border: "#FFE600" } },
     ]);
 
     // Criar arestas
     const edges = new vis.DataSet([
         { from: 1, to: 2, color: { color: "#FFE600" } },
         { from: 1, to: 3, color: { color: "#FFE600" } },
-        { from: 1, to: 4, color: { color: "#FFE600" } },
         { from: 3, to: 4, color: { color: "#FFE600" } },
         { from: 6, to: 5, color: { color: "#FFE600" } },
     ]);
--- Interface-Inicial/script.js
+++ Interface-Inicial/script.js
@@ -1,28 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-const caneta = document.getElementById("btn-caneta");
+  const grafosBtn = document.getElementById("grafos-btn");
+  const decksBtn = document.getElementById("btn-decks");
+  const caneta = document.getElementById("btn-caneta");
+  const calendarioBtn = document.getElementById("calendario-btn");
+
 
   if (caneta) {
     caneta.addEventListener("click", function () {
       window.location.href = "../Criacao-nota/index.html";
     });
   }
-  
-  // Controle do menu lateral
-  const menuToggle = document.getElementById("menuToggle");
-  const sidebar = document.getElementById("sidebar");
 
-  menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
 
-    const icon = menuToggle.querySelector("i");
-    if (sidebar.classList.contains("active")) {
-      icon.classList.remove("fa-bars");
-      icon.classList.add("fa-times");
-    } else {
-      icon.classList.remove("fa-times");
-      icon.classList.add("fa-bars");
-    }
-  });
+  if (grafosBtn) {
+    grafosBtn.addEventListener("click", function () {
+      window.location.href = "../Anotacoes-grafos/index.html";
+    });
+  }
+
+  if (decksBtn) {
+    decksBtn.addEventListener("click", function () {
+      window.location.href = "../Interface-Flahscard/interface.flashcard1.html";
+    });
+  }
+
+   if (calendarioBtn) {
+    calendarioBtn.addEventListener("click", function () {
+      window.location.href = "../Calendario/calendario.html";
+    });
+  };
+
 
   // Fecha o menu ao clicar em um item (modo mobile)
   const sidebarItems = document.querySelectorAll(".sidebar-item");
